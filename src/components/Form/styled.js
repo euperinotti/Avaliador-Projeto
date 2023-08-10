49992282e8443b9422c32c9363bc6ccfbc954f2f
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-08-10 16:39:25 -0300

    componente formulario

---
+++ src/components/Form/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 1rem;
+  border-radius: 50px;
+  width: fit-content;
+  height: auto;
+  margin: 50px;
+`
