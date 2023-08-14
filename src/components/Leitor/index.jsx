8ffe406885017f561c7ac08a3165f070a0d66acb
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-08-14 14:23:36 -0300

    ajustes no componente de leitor

--- src/components/Leitor/index.jsx
+++ src/components/Leitor/index.jsx
@@ -1,30 +1,33 @@
 import { useEffect, useState } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import * as S from './styles'
 
 import "./styles.css";
+import { Base } from "../Base";
 
 export default function Leitor() {
   const [scan, setScan] = useState(true);
-  const [logs, setLog] = useState<Array<string>>([]);
+  const [logs, setLog] = useState < Array < string >> ([]);
 
-  const barcodeScannerComponentHandleUpdate = (error: any, result: any) => {
+  const barcodeScannerComponentHandleUpdate = (error, result) => {
     if (result) {
       setLog([...logs, result.text]);
       setScan(false);
     }
   };
-  useEffect(()=>{
-    if(window.sessionStorage.getItem('id') === null || window.sessionStorage.getItem('acesso') != 'avaliador'){
+  useEffect(() => {
+    if (window.sessionStorage.getItem('id') === null || window.sessionStorage.getItem('acesso') != 'avaliador') {
       window.location.href = '/'
     }
   });
 
   return (
-    <div className="App">
-
+    <Base>
       {scan && (
         <div>
           <BarcodeScannerComponent
+           width={500}
+           height={500}
             onUpdate={barcodeScannerComponentHandleUpdate}
           />
         </div>
@@ -36,8 +39,7 @@ export default function Leitor() {
 
           window.location.href = '/questao'
         })}
-
       </div>
-    </div>
+    </Base>
   );
 }
