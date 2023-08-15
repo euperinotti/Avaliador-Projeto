d288b98cb9e01a8be81e248d3dd62db01888f49a
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-08-15 13:41:55 -0300

    ajustes componente leitor

--- src/components/Leitor/index.jsx
+++ src/components/Leitor/index.jsx
@@ -1,40 +1,18 @@
-import { useEffect, useState } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
-import * as S from './styles'
+import * as S from  './styles'
 
-import { Base } from "../Base";
+export const Leitor = () => {
 
-export default function Leitor() {
-  const [scan, setScan] = useState(true);
-  const [logs, setLog] = useState("");
-
-  const barcodeScannerComponentHandleUpdate = (error, result) => {
+  const handleUpdate = (error, result) => {
     if (result) {
-      setLog(result.text);
+      setLog([...logs, result.text]);
       setScan(false);
-
-      window.sessionStorage.setItem('trabalho', logs)
-
-          window.location.href = '/questao'
     }
   };
-  useEffect(() => {
-    if (window.sessionStorage.getItem('id') === null || window.sessionStorage.getItem('acesso') != 'avaliador') {
-      window.location.href = '/'
-    }
-  });
 
   return (
-    <Base>
-      {scan && (
-        <div>
-          <BarcodeScannerComponent
-           width={500}
-           height={500}
-            onUpdate={barcodeScannerComponentHandleUpdate}
-          />
-        </div>
-      )}
-    </Base>
-  );
+    <BarcodeScannerComponent
+      onUpdate={handleUpdate}
+    />
+  )
 }
