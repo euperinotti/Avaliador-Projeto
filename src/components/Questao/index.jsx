1c6acef061743a0fb786138bc6aba2b21e3af42a
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-08-17 15:59:52 -0300

    componente questao

--- src/components/Questao/index.jsx
+++ src/components/Questao/index.jsx
@@ -1,23 +1,17 @@
 import { Datalist } from '../Datalist';
+import { InputRange } from '../InputRange';
 import * as S from './styles'
 
 export const Questao = ({ titulo, label, value, onChange }) => {
   return (
-    <S.Text>
-      {titulo}
-      <br />
-      <br />
-      <input
-        type="range"
-        min={0}
-        max={10}
+    <S.Container>
+      <S.Text>{titulo}</S.Text>
+      <InputRange
         label={label}
         value={value}
         onChange={onChange}
-        list="tickmarks"
-        className="slider"
       />
       <Datalist />
-    </S.Text>
+    </S.Container>
   )
 }
