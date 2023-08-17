6d2497d23ad3b61673569bfca50e643a28538bdb
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-08-17 14:39:42 -0300

    componente datalist

---
+++ src/components/Datalist/index.jsx
@@ -0,0 +1,20 @@
+import * as S from './styles'
+import { Option } from "../Option"
+
+export const Datalist = () => {
+  return (
+    <S.Datalist>
+      <Option value="0">0</Option>
+      <Option value="1">.</Option>
+      <Option value="2">.</Option>
+      <Option value="3">.</Option>
+      <Option value="4">.</Option>
+      <Option value="5">5</Option>
+      <Option value="6">.</Option>
+      <Option value="7">.</Option>
+      <Option value="8">.</Option>
+      <Option value="9">.</Option>
+      <Option value="10">10</Option>
+    </S.Datalist>
+  )
+}
---
+++ src/components/Datalist/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Datalist = styled.datalist`
+  margin: 1rem 0;
+`
