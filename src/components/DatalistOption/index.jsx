7b0cbf4aa745b6251ff45cc8386bdb627d97a75b
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-08-17 14:52:56 -0300

    componente option para datalist

---
+++ src/components/DatalistOption/index.jsx
@@ -0,0 +1,5 @@
+import * as S from './styles'
+
+export const DatalistOption = () => {
+  return <S.Option></S.Option>
+}
---
+++ src/components/DatalistOption/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Option = styled.option`
+  padding-left:6px;
+  font-size:0.7rem;
+`
