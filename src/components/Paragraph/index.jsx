7b200b05f559384f0fb580919865797540413c45
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-08-17 13:57:02 -0300

    componente paragrafo

---
+++ src/components/Paragraph/index.jsx
@@ -0,0 +1,7 @@
+import * as S from './styles'
+
+export const Paragraph = ({ children }) => {
+  return (
+    <S.Paragraph>{children}</S.Paragraph>
+  )
+}
---
+++ src/components/Paragraph/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Paragraph = styled.p`
+  color: white;
+  font-size: medium;
+  text-align: center;
+  width: 70%;
+`
