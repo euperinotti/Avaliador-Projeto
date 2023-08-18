4b1209e91409d8d5d8e5c242aefa28536e43b586
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-08-18 16:56:32 -0300

    update

--- src/pages/votacao-aberta.jsx
+++ src/pages/votacao-aberta.jsx
@@ -7,14 +7,12 @@ import { Heading } from "../components/Heading";
 import { Paragraph } from "../components/Paragraph";
 import { Select } from "../components/Select";
 import { Logo } from "../components/Logo";
+import { auth, AuthPopup } from "../auth";
 
 const VotacaoAberta = () => {
-
-  if (!window.sessionStorage.getItem('token')) {
-    window.location.href = '/'
-  }
-
   const [projetos, setProjetos] = useState([]);
+  const [podeVotar, setPodeVotar] = useState(true)
+  const authUser = auth()
 
   const handlerSubmit = async (e) => {
     e.preventDefault();
@@ -25,14 +23,16 @@ const VotacaoAberta = () => {
   };
 
   useEffect(async () => {
-    await participante();
-    await checkVoto();
+    try {
+      await participante();
+      await checkVoto();
+    } catch (e) {
+      console.log(e);
+    }
   }, []);
-
 
   const participante = async () => {
     const json = await axiosParticipantes()
-
     const projetos = json.data.participantes;
 
     setProjetos(projetos);
@@ -40,12 +40,10 @@ const VotacaoAberta = () => {
 
   const checkVoto = async () => {
     const json = await axiosCheckVoto(window.sessionStorage.getItem('id'))
-
     const { codigo } = json.data;
 
     if (codigo === 200) {
-      alert('Só pode ser realizado 1 voto por usuario.');
-      window.location.href = '/';
+      setPodeVotar(false)
     }
   };
 
@@ -66,23 +64,21 @@ const VotacaoAberta = () => {
 
   return (
     <Base>
+    {!authUser.status && (<AuthPopup message={authUser.message} />)}
+    {!podeVotar && (<AuthPopup message={"Você já votou"} />)}
       <Logo />
       <Heading>Votação Popular</Heading>
-        <Paragraph>
-          Selecione o projeto que deseja enviar seu voto e depois clique em
-          enviar voto.
-          <br />
-          <br />
-          <strong>Cada usuário tem direito a 1 voto.</strong>
-        </Paragraph>
+      <Paragraph>
+        Selecione o projeto que deseja enviar seu voto e depois clique em
+        enviar voto.
+        <br />
+        <br />
+        <strong>Cada usuário tem direito a 1 voto.</strong>
+      </Paragraph>
       <Form onSubmit={e => handlerSubmit(e)}>
-        
         <Select label="produtos" name="categoria1" data={projetos} categoria="1" />
-
         <Select label="soluções" name="categoria2" data={projetos} categoria="2" />
-
         <Select label="aplicativos" name="categoria3" data={projetos} categoria="3" />
-
         <Button>Enviar Voto</Button>
       </Form>
     </Base>
