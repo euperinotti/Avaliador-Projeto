682f4952ac72753e68586f7703cd8559822ef586
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-08-18 15:03:59 -0300

    adicionado global styles

---
+++ src/globalStyles.js
@@ -0,0 +1,41 @@
+const theme = {
+  colors: {
+    main: "#2b3643",
+    second: "#323E4D",
+    third: "#485A70",
+    blue: "#228CC8"
+  },
+  font: {
+    size: {
+      small: "1rem",
+      medium: "1.5rem",
+      large: "2rem",
+      xlarge: "2.5rem"
+    }
+  },
+  align: {
+    flexCenter: {
+      "display": "flex",
+      "justify-content": "center",
+      "align-items": "center"
+    }
+  },
+  dimensions: {
+    viewHeight: "100vh",
+    viewWidth: "100vw",
+    allDisplay: "100%",
+  },
+  spacing: {
+    small: "1rem",
+    medium: "2rem",
+    large: "2.5rem",
+    xlarge: "3rem"
+  },
+  misc: {
+    roundCorners: {
+      "border-radius":"35px"
+    }
+  }
+}
+
+export default theme
